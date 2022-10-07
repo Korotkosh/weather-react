004d08c4290cda18bc0ab5a0b99737aafc61c0f3
Korotkosh <108016188+Korotkosh@users.noreply.github.com>
2022-10-07 10:20:38 +0300

    adedd link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <a
+        href="https://github.com/Korotkosh/weather-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        GitHub repository
+      </a>
     </div>
   );
 }
